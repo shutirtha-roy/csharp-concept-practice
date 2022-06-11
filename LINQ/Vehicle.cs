8e2832f0b9fe286ce4d61442e73c9642d4580407
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-12 01:11:34 +0600

    Vehicle Class Implemented

---
+++ LINQ/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class Vehicle
+    {
+        public int VehicleId { get; set; }
+        public string VehicleName { get; set; } 
+        public int Weight { get; set; }
+    }
+}
