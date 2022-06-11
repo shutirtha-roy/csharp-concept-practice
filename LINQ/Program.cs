3eddc4dbb9ef7d73b4ca942618d426353951f500
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-12 00:46:00 +0600

    Basic LINQ Query and Method Syntax Added

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LINQ
 {
@@ -6,7 +7,21 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Basic Concepts
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+
+            //LINQ Query Syntax
+            Console.WriteLine("Query Syntax");
+            var evenNumbers = (from num in numbers
+                               where num % 2 == 0
+                               select num).ToArray();
+            Console.WriteLine($"Even numbers are {string.Join(" ", evenNumbers)}");
+
+            //LINQ Method Syntax
+            Console.WriteLine("Method Syntax");
+            var oddNumbers = numbers.Where(num => num % 2 == 1).ToArray();
+            Console.WriteLine($"Odd numbers are {string.Join(" ", oddNumbers)}");
         }
     }
 }
