99cb85a06705ec93dc9340ecb21c8aaea274619e
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-13 00:36:29 +0600

    WHERE clause implemented

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -14,6 +14,7 @@ namespace LINQ
         {
             //Basic Concepts
             int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+            //https://www.tutorialsteacher.com/linq/linq-filtering-operators-where
 
             # region Basic LINQ Query Syntax
             //Console.WriteLine("Query Syntax");
@@ -27,11 +28,9 @@ namespace LINQ
             var oddNumbers = numbers.Where(num => num % 2 == 1).ToArray();
             //Console.WriteLine($"Odd numbers are {string.Join(" ", oddNumbers)}");
             #endregion
-
             #region WHERE clause
-            Console.WriteLine("WHERE clause");
-            //Query Syntax
-            Console.WriteLine("WHERE Query Syntax clause");
+            //Console.WriteLine("WHERE clause");
+            
 
             List<Vehicle> vehicles = new List<Vehicle>()
             {
@@ -42,22 +41,49 @@ namespace LINQ
                 new Vehicle() { VehicleId = 5, VehicleName = "Tata", Weight = 45 }
             };
 
+            //Query Syntax
+            //Console.WriteLine("WHERE Query Syntax clause");
+
             var seperatedVehicles = (from vehicle in vehicles
                                      where vehicle.Weight > 30 && vehicle.Weight <= 50
                                      select vehicle.VehicleName).ToArray();
 
-            Console.WriteLine($"Seperated Vehicle: {string.Join("->", seperatedVehicles)}");
+            //Console.WriteLine($"Seperated Vehicle: {string.Join("->", seperatedVehicles)}");
 
             var seperatedVehiclesMethod = (from vehicle in vehicles
                                            where isLight(vehicle)
                                            select vehicle).ToArray();
 
-            foreach(var vehicle in seperatedVehiclesMethod)
+            //foreach(var vehicle in seperatedVehiclesMethod)
+            //{
+            //    Console.WriteLine($"{vehicle.VehicleId} {vehicle.VehicleName} {vehicle.Weight}");
+            //}
+
+            //Multiple Where Clause
+            var newSeperatedVehicles = (from vehicle in vehicles
+                                     where vehicle.Weight > 30
+                                     where vehicle.Weight <= 50
+                                     select vehicle.VehicleName).ToArray();
+
+            //Method Syntax
+            //Console.WriteLine("Method Syntax");
+            var seperatedMethodVehicles = vehicles.Where(v => v.Weight > 30 && v.Weight <= 50);
+
+            var seperatedResult = vehicles.Where((s, i) =>
             {
-                Console.WriteLine($"{vehicle.VehicleId} {vehicle.VehicleName} {vehicle.Weight}");
-            }
+                if (i % 2 == 0)
+                    return true;
+
+                return false;
+            });
+
+            //Multiple WHERE Clause
+            var newMethodSeperatedResult = vehicles.Where(v => v.Weight > 30).Where(v => v.Weight <= 50);
+
 
             #endregion
+
+
         }
     }
 }
