4eaab63380fad2f561ef9b953fb53428ef89582b
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-16 03:00:48 +0600

    Tool Class Added and Implemented

---
+++ LINQ/Tools.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class Tools
+    {
+        public int ToolId { get; set; }
+        public string ToolName { get; set; }
+        public int GeneralId { get; set; }
+    }
+}
