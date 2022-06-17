ab186b37c14dd50cc67dd243922a072667345f5e
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-18 02:13:47 +0600

    Passing Delegate as a Parameter Added

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -4,9 +4,39 @@ namespace Delegates
 {
     class Program
     {
-        static void Main(string[] args)
+        public delegate int Operation(int x, int y);
+        public delegate void MyDelegate(string msg);
+
+        public static int Addition(int a, int b)
         {
-            Console.WriteLine("Hello World!");
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return Math.Abs(a - b);
+        }
+
+        public static void Main(string[] args)
+        {
+            #region Passing Delegate as a Parameter
+            //Operation newOperationAddition = new Operation(Addition);
+            Operation newOperationAddition = Addition;
+            //newOperationAddition = (int a, int b) => a + b;
+            //Console.WriteLine($"Addition: {newOperationAddition.Invoke(5, 2)}");
+
+            //Operation newOperationSubtration = new Operation(Subtraction);
+            Operation newOperationSubtration = Subtraction;
+            //newOperationSubtration = (int a, int b) => a - b;
+            //Console.WriteLine($"Subtration: {newOperationSubtration.Invoke(5, 2)}");
+            #endregion
+            #region Delegate
+
+
+            #endregion
+
+
+
         }
     }
 }
