f5958d0fef7b6d15b6c27c54335879a960ffb25e
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-18 02:31:05 +0600

    Multicast Delegate Returning a Value Added

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -6,6 +6,7 @@ namespace Delegates
     {
         public delegate int Operation(int x, int y);
         public delegate void MyDelegate(string msg);
+        public delegate int MyDelegateInt();
 
         public static int Addition(int a, int b)
         {
@@ -15,6 +16,26 @@ namespace Delegates
         public static int Subtraction(int a, int b)
         {
             return Math.Abs(a - b);
+        }
+
+        public static void FunnyMessageMethod(string msg)
+        {
+            Console.WriteLine("Let's Laugh: " + msg);
+        }
+
+        public static void AngryMessageMethod(string msg)
+        {
+            Console.WriteLine("You are angry: " + msg);
+        }
+
+        public static int HundredMethod()
+        {
+            return 100;
+        }
+
+        public static int TwoHundredMethod()
+        {
+            return 200;
         }
 
         public static void Main(string[] args)
@@ -30,11 +51,34 @@ namespace Delegates
             //newOperationSubtration = (int a, int b) => a - b;
             //Console.WriteLine($"Subtration: {newOperationSubtration.Invoke(5, 2)}");
             #endregion
-            #region Delegate
+            #region Multicast Delegate
+            MyDelegate deligateFunnyMessage = FunnyMessageMethod;
+            MyDelegate deligateAngryMessage = AngryMessageMethod;
+
+            MyDelegate deligateFunnyAngryMessage = deligateFunnyMessage + deligateAngryMessage;
+            //deligateFunnyAngryMessage("Roy");
+
+            MyDelegate deligateCoolMessage = (string msg) =>
+            {
+                Console.WriteLine("Cool");
+                Console.WriteLine("It is fun");
+            };
+
+            deligateFunnyAngryMessage += deligateCoolMessage;
+            //deligateFunnyAngryMessage("Roy");
+
+            deligateFunnyAngryMessage -= deligateFunnyMessage;
+            //deligateFunnyAngryMessage("Roy");
+            #endregion
+            #region Multicast Delegate Returning a Value
+            MyDelegateInt hundred = HundredMethod;
+            MyDelegateInt twoHundred = TwoHundredMethod;
+
+            MyDelegateInt threeHundred = hundred + twoHundred;
+            Console.WriteLine(threeHundred());
 
 
             #endregion
-
 
 
         }
