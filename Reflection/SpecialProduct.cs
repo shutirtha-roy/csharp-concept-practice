7fa4ccc1ee693fc7a169ce25a0bac2c087f23033
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-20 20:10:26 +0600

    SpecialProduct Class Added

---
+++ Reflection/SpecialProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class SpecialProduct : Product
+    {
+        public override double PriceAfterDiscount(double discount)
+        {
+            return Price;
+        }
+    }
+}
