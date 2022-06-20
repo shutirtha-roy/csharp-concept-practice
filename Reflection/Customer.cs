cd70eddcbf01d4693030acb9cae7dce4520e3c30
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-20 20:09:47 +0600

    Customer Class Added

---
+++ Reflection/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Customer(int ID, string Name)
+        {
+            this.Id = ID;
+            this.Name = Name;
+        }
+
+        public Customer()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+        }
+
+        public void PrintID()
+        {
+            Console.WriteLine("ID = {0}", this.Id);
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine("Name = {0}", this.Name);
+        }
+    }
+}
