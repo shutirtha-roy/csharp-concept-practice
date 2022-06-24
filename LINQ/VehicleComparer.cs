d3e834aab67a0109edae3c2dc580c80c6844d530
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-25 02:06:06 +0600

    VehicleComparer Added

---
+++ LINQ/VehicleComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class VehicleComparer : IEqualityComparer<Vehicle>
+    {
+        public bool Equals(Vehicle x, Vehicle y)
+        {
+            if (x.VehicleId == y.VehicleId && x.VehicleName.ToLower() == y.VehicleName.ToLower())
+                return true;
+            else
+                return false;
+        }
+
+        public int GetHashCode(Vehicle obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
