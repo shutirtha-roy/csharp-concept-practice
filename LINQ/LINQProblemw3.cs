b5a51f197be3e39049ee05e612e16d2b1d0ec9b1
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-28 06:14:57 +0600

    Excercise3 Added

--- LINQ/LINQProblemw3.cs
+++ LINQ/LINQProblemw3.cs
@@ -26,5 +26,57 @@ namespace LINQ
                               select value)
                              );
         }
+        public void Excercise2()
+        {
+            //Write a program in C# Sharp to find the +ve numbers from a
+            //list of numbers using two where conditions in LINQ Query.
+            //Expected Output:
+            //The numbers within the range of 1 to 11 are:
+            //1 3 6 9 10
+            IList<int> values = Enumerable.Range(-11, 20).ToList();
+            //Console.WriteLine($"Excercise 2: {string.Join(" ", values.Where(value => value > 0).ToList())}");
+            Console.WriteLine(string.Join(" ", from value in values
+                                               where value > 0
+                                               select value));
+        }
+        public void Excercise3()
+        {
+            //Write a program in C# Sharp to find the number of an array 
+            //and the square of each number. Go to the editor
+            //Expected Output:
+            //{ Number = 9, SqrNo = 81 }
+            //{ Number = 8, SqrNo = 64 }
+            //{ Number = 6, SqrNo = 36 }
+            //{ Number = 5, SqrNo = 25 }
+            int[] digits = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var newDigits = from digit in digits
+                            select new
+                            {
+                                Number = digit,
+                                SqrNo = (digit << 2)
+                            };
+
+            //foreach(var digit in newDigits)
+            //{
+            //    Console.WriteLine(digit);
+            //}
+
+            var newDigits1 = from digit in digits
+                                let SqrNo = (digit << 2)
+                                //let index = digit - 1
+                                select new 
+                                {
+                                    digit, 
+                                    SqrNo ,
+                                    //index
+                                };
+
+            foreach (var digit in newDigits1)
+            {
+                Console.WriteLine(digit);
+            }
+
+        }
     }
 }
