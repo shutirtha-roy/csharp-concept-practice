c110f04dc1c7586f3d20a09c92e55f30b500dc7f
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-07-01 03:49:06 +0600

    Events Added

--- Events/Program.cs
+++ Events/Program.cs
@@ -4,9 +4,28 @@ namespace Events
 {
     class Program
     {
+        delegate void Receive(string recepient, string message);
+        static event Receive Subscribe;
+
+        private static void MMSSubscriber(string recepient, string message)
+        {
+            Console.WriteLine($"Sending MMS to -> {recepient}, message -> {message}");
+        }
+
+        private static void GmailSubscriber(string recepient, string message)
+        {
+            Console.WriteLine($"Sending email to -> {recepient}, message -> {message}");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Subscribe += MMSSubscriber;
+            Subscribe += GmailSubscriber;
+
+            //Subscribe.Invoke("samin", "apurba");
+
+            Subscribe -= MMSSubscriber;
+            Subscribe.Invoke("samin", "apurba");
         }
     }
 }
