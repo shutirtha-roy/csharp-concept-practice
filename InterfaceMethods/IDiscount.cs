0c7b10b581a8d8102415c55e11813d449f1418bd
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-07-02 02:15:49 +0600

    IDiscount Added

---
+++ InterfaceMethods/IDiscount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceMethods
+{
+    public interface IDiscount
+    {
+        double CalculateAmount(double originalAmount);
+        public double GetAmount()
+        {
+            return 0;
+        }
+    }
+}
