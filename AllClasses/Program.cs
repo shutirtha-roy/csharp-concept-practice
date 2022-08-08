560793bae4fafac41390c090d4596f140b951a85
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-08-08 19:16:34 +0100

    Two student instance were added

--- AllClasses/Program.cs
+++ AllClasses/Program.cs
@@ -6,7 +6,19 @@ namespace AllClasses
     {
         static void Main(string[] args)
         {
-            
+            Student student = new Student();
+            student.name = "Roy";
+            student.cgpa = 3.4;
+            student.address = "Dhaka";
+            student.dateOfBirth = new DateTime(1999, 2, 2);
+
+            student.UpdateCgpa(3.76);
+
+
+            Student student2 = new Student();
+            student2.cgpa = 3.5;
+            student2.name = "Samin";
+            student2.UpdateCgpa(3.99);
 
         }
     }
