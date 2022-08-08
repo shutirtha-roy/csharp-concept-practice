f6da1c3700174e7dd029060c7226c19b033d2087
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-08-08 21:39:21 +0100

    Static class Implemented

---
+++ AllClasses/AreaCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllClasses
+{
+    public static class AreaCalculator
+    {
+        public static int count;
+        public static string Name { get; set; }
+
+        public static double GetCircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public static double GetRectangle(double width, double height)
+        {
+            return width * height;
+        }
+    }
+}
