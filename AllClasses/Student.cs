fe6a4b09af7d299442cd482ec1b848e63b165ce8
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-08-08 20:42:14 +0100

    Constructor and Method Overloading Added

--- AllClasses/Student.cs
+++ AllClasses/Student.cs
@@ -24,5 +24,52 @@ namespace AllClasses
                     cgpa = value;
             }
         }
+
+        //When there is no constructor it can be called invisible default constructor
+
+        //Empty Constructor
+        public Student()
+        {
+            Name = string.Empty;
+            cgpa = 0;
+            Address = string.Empty;
+            DateOfBirth = DateTime.MinValue;
+        }
+
+        public Student(string name, double cgpa, string address, DateTime dateofBirth)
+        {
+            Name = name;
+            Address = address;
+            DateOfBirth = dateofBirth;
+        }
+
+        //Method overloading
+        public void UpdateDetails(DateTime dateOfBirth)
+        {
+            DateOfBirth = dateOfBirth;
+        }
+        public void UpdateDetails(string name, string address)
+        {
+
+        }
+        public void UpdateDetails(string name, DateTime dateOfBirth)
+        {
+
+        }
+        public void UpdateDetails(DateTime dateOfBirth, string name)
+        {
+
+        }
+
+        //More Examples
+        //We can add one product or multiple products
+        //public void AddProduct(Product product)
+        //{
+
+        //}
+        //public void AddProduct(Product[] products)
+        //{
+
+        //}
     }
 }
