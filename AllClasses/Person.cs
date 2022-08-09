ef89585e075e8f1f92e821730a7ea392a131e629
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-08-09 21:33:59 +0100

    Common properties and methods of Student and Teacher Added

--- AllClasses/Person.cs
+++ AllClasses/Person.cs
@@ -8,6 +8,20 @@ namespace AllClasses
 {
     public class Person
     {
-        
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string IdPrefix;
+        public string GenerateId()
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                return $"TC-{Name.Substring(0, 2)}";
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
