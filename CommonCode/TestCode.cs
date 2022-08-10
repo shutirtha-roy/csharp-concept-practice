842788be154ce986f528957ff3d0becee3d5403f
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-08-10 12:56:22 +0100

    TestCode implemented to check if it is able to access internal Description property

--- CommonCode/TestCode.cs
+++ CommonCode/TestCode.cs
@@ -8,6 +8,10 @@ namespace CommonCode
 {
     public class TestCode
     {
-        
+        public void Test()
+        {
+            Course course = new Course();
+            course.Description = "Test";
+        }
     }
 }
