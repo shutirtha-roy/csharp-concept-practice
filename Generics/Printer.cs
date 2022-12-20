bee31356b060b72745ed207fddf6d4ac78b1daf4
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-12-20 21:32:03 +0600

    Printer static class Implemented

---
+++ Generics/Printer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public static class Printer<X>
+    {
+        public static void Print<T>(T item) where T : class
+        {
+            T x = default(T);
+            X a = default(X);
+            Console.WriteLine(item.ToString());
+        }
+
+        public static void Revert()
+        {
+            //T t = default(T);
+            X x = default(X);
+        }
+    }
+}
