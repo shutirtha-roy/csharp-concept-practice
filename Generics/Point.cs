23bdf02878e7032b858469d47f5059f4f0cd59fe
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-12-20 21:06:44 +0600

    Point class Implemented

---
+++ Generics/Point.cs
@@ -0,0 +1,13 @@
+﻿namespace Generics
+{
+    public class Point<T, D>
+    {
+        public T X;
+        public D Y;
+
+        public string Sum(T X, D Y)
+        {
+            return X.ToString() + Y.ToString(); 
+        }
+    }
+}
