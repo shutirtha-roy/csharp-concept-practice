9244d24921d653da8dc58bb209067bf02714689c
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-12-20 21:19:35 +0600

    region set for Point object calls

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,6 +4,18 @@
     {
         public static void Main(string[] args)
         {
+            #region Point
+            //var pointOne = new Point<int, double>();
+            //pointOne.X = 5;
+            //pointOne.Y = 5.5;
+
+            //var result = pointOne.X + pointOne.Y;
+            //Console.WriteLine(result);
+
+            //Now we are add Item as it implemented ISummable
+            //var pointTwo = new Point<int, Item>();
+            #endregion
+
 
         }
     }
